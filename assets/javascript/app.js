cf70c7636362f75f048dbc75a6908a4b136975d6
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2019-10-30 11:16:06 -0400

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+
+
+
+
+// initialize firebase
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBgcJnPCVGQE8AGfN9Jsf2lzzraMo8kq1g",
+    authDomain: "newproject-d8376.firebaseapp.com",
+    databaseURL: "https://newproject-d8376.firebaseio.com",
+    projectId: "newproject-d8376",
+    storageBucket: "newproject-d8376.appspot.com",
+    messagingSenderId: "101423244744",
+    appId: "1:101423244744:web:72fca8e69ffb8e7b6a0cfd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
